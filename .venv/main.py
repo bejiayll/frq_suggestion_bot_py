2f6082cd18fcef2c38e88c83c7a0eaa28fac522e
ImNotForq <neon20.xr@gmail.com>
2025-02-03 21:06:43 +0400

    Add verification

--- .venv/main.py
+++ .venv/main.py
@@ -5,6 +5,7 @@ import configpy.jsoncontrolleer as jsc
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
 import aiogram
@@ -21,11 +22,19 @@ dp = Dispatcher()
 
 # Functions
 
-def ismod(id : str) -> bool:
-    return id in jsc.load("mods.json")
-
 def user_in_base(id: str) -> bool:
-    return id in jsc.load("data.json")
+    return str(id) in jsc.load("data.json")
+
+def is_mod(id : str) -> bool:
+    if str(id) in jsc.load("data.json"):
+        return jsc.load("data.json")[str(id)]["permissions"] >= 1
+    else: return False
+
+
+def is_superuser(id: str) -> bool:
+    if str(id) in jsc.load("data.json"):
+        return jsc.load("data.json")[str(id)]["permissions"] > 1
+    else: return False
 
 # Commands handler
 
@@ -43,19 +52,71 @@ async def com_start(message: types.Message):
     await bot.send_sticker(message.from_user.id, sticker="CAACAgIAAxkBAAEMGcRnnNSfm088IIxaIgNmYZZcZFRLFgACpFcAAoH6YEtxYB8QQkhdCTYE")
 
 
-@dp.message(Command("verif"))
+@dp.message(Command("verify"))
 async def com_verif(message: types.Message):
+
+    '''
+    syntax:
+    /verify <user_1 id: Integer> <user_2 id: Integer> ... <user_N id: Integer>
+    '''    
     
-    if ismod(message.from_user.id):
+    if is_mod(str(message.from_user.id)) or is_superuser(str(message.from_user.id)):
         args = message.text.split()
+        print(args)
         if len(args) < 2:
-            await message.answer("Укажите хотябы одного пользователя для верификации"); return
+            await message.answer("Укажите id хотябы одного пользователя для верификации."); return
         
         counter = 0
         for id in args:
-            if (isinstance(id, int)) and (id in jsc.load("data.json")) and (jsc.load("data.json")[id]["status"] < 1):
-                ...
-                
+            if (user_in_base(id)) and (jsc.load("data.json")[id]["status"] < 1):
+                data = jsc.load("data.json")
+                data[id]["status"] = 1
+                jsc.dump("data.json", data)
+                counter += 1
+        await message.answer(f"Верифицированно пользователей: {counter}.")
+
+    else:
+        await message.answer("У вас недостаточно прав для выполнения этой команды.")
+
+
+@dp.message(Command("set_value"))
+async def com_set_value(message: types.Message):
+
+    
+    '''
+    syntax:
+    /set_value <user id : Integer> <edit value : String> <set value : Integer from 0 to 2>
+    Allowed values:
+        status:
+            0 - unverified
+            1 - verified
+            2 - banned 
+    
+        permissions:
+            0 - user (no permissions)
+            1 - moderator (can verify users)
+            2 - superuser(all permissions)
+    '''
+
+    args = message.text.split()
+    if is_superuser((str)(message.from_user.id)):
+        try:
+            user = args[1]
+            value = args[2]
+            to_value = int(args[3])
+
+            data = jsc.load("data.json")
+            data[id][value] = to_value
+
+            await message.answer(f"Изменено значение {value} для {user} на {to_value}")
+
+        except:
+            await message.answer("Что то пошло не так")
+
+    
+@dp.message(Command("debug_im_superuser"))
+async def debug_com_user_is_superuser(message: types.Message):
+    await message.answer(str(is_superuser(str(message.from_user.id))))
 
 # Buttons handler
 
@@ -99,12 +160,14 @@ async def account_handler(message: types.Message):
 async def account_status_handler(message: types.Message):
     dct = jsc.load("data.json")
     author_id = (str)(message.from_user.id)
-    author_status_id = dct[author_id]["status"]
-    author_perm_id = dct[author_id]["permissions"]
 
     if author_id in dct:
+
+        author_status_id = dct[author_id]["status"]
+        author_perm_id = dct[author_id]["permissions"]
+
         match author_status_id:
-            case 0 : status = "❓Прохоит верификацию"
+            case 0 : status = "❓Ожидает верификацию"
             case 1 : status = "✅Верифицирован"
             case 2 : status = "📛Заблокирован"
         match author_perm_id:
@@ -115,7 +178,36 @@ async def account_status_handler(message: types.Message):
         await message.answer(f"Ваш статус верификации: {status}\nВаши права: {perm}")
 
     else:
-        await message.answer("Вы не в базе, нажмите inline-кнопку чтобы отправить заявку на верификацию")
+
+        inline_builder = InlineKeyboardBuilder()
+        inline_builder.add(InlineKeyboardButton(text = "Отправить заявку на верификацию.", callback_data = "verify_req"))
+        await message.answer("Вы не в базе, нажмите на кнопку под сообщением чтобы отправить заявку на верификацию", reply_markup= inline_builder.as_markup())
+
+@dp.message(F.text.lower() == "f.a.q.")
+async def faq_handler(message: types.Message):
+    await message.answer("W.I.P.")
+
+# Callback
+
+@dp.callback_query(lambda call: call.data == "verify_req")
+async def callback_req_verify(callback_query: types.CallbackQuery):
+    user = callback_query.from_user.id
+    user_name = callback_query.from_user.full_name
+    
+    if not user_in_base(str(user)):
+        for u in jsc.load("data.json"):
+            if is_mod(u): await bot.send_message(int(u), f"Пользователь {user_name} (`{user}`) подал заявку на верификацию.")
+        
+        await bot.send_message(user, "Ваша заявка была отправленна.")
+        base = jsc.load("data.json")
+        base[user] = {"status" : 0, 
+                      "permissions" : 0}
+        jsc.dump("data.json", base)
+    
+    else: 
+        await bot.send_message(user, "Вы уже отправляли заявку, проверьте статус вашей верификации и при необходимости свяжитесь с администратором.")
+
+
 
 ###=================###
 
